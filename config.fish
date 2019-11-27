5532d7220f36e5c542e8d5de8b4fb1abb6eaecbd
yukio <fenrirpride@gmail.com>
2019-11-27 16:36:26 +0900

    add

--- config.fish
+++ config.fish
@@ -4,7 +4,7 @@ set -g fish_user_paths "/home/linuxbrew/.linuxbrew/bin" $fish_user_paths
 # to update
 alias update='sudo apt update; sudo apt -y upgrade; sudo apt -y full-upgrade; sudo apt -y autoclean; sudo apt -y autoremove;flatpak update;snap refresh;brew update;brew upgrade'
 # output code extensions
-alias code_out='code --list-extensions > ~/dotfiles/vscode/extensions_list.txt'
+alias code_out='code --list-extensions > ~/dotfiles/Code/extensions_list.txt'
 # ghq
 set GHQ_SELECTOR peco
 set -U FZF_LEGACY_KEYBINDINGS 0
