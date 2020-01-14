e50563499877222007726c45d7baee471dff8a36
fenrirpride <fenrirpride@gmail.com>
2020-01-14 22:43:34 +0900

    change epub command
    add notify when no updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,7 +6,9 @@ set -U FZF_LEGACY_KEYBINDINGS 0
 # set PATH
 set -x PATH /home/linuxbrew/.linuxbrew/bin $PATH
 set -x PATH ~/go/bin $PATH
+
 # functions
+# system
 function update -d "update all packages"
     sudo apt update
     sudo apt -y upgrade
@@ -18,35 +20,44 @@ function update -d "update all packages"
     brew update
     brew upgrade
 end
-# output code extensions
+
+# vscode
 function code_out -d "make list vscode extensions"
     code --list-extensions > ~/MyCode/dotfiles/Code/extensions_list.txt
 end
 function code_in -d "install vscode extensions"
     sh ~/MyCode/dotfiles/.config/fish/code_in.sh
 end
-# view code extensions
+
 function view_ex -d "view list vscode extensions"
     cat ~/MyCode/dotfiles/Code/extensions_list.txt
 end
+
 # narou
 function narouu -d "narou update"
     cd /mnt/DATA1/Novel
     narou u
     cd
 end
+
 function naroud -d "narou download"
     cd /mnt/DATA1/Novel
     narou d $argv
     cd
 end
+
 function epub -d "epub upload dropbox"
     find /mnt/DATA1/Novel/ -name \*.epub | while read line
     cp -vu "$line" /mnt/DATA1/online/Dropbox/epub | awk -F'[\''/']' -v 'ORS=\n' '{print $9}'
     cp -u "$line" ~/pCloudDrive/epub
     end
-    echo -e "\e[32mupdated!\e[m"
+    if string length -q -- $line
+        echo -e "\e[32m"Update completed!"\e[m"
+    else
+        echo -e "\e[32m"No updates!"\e[m"
+    end
 end
+
 # backup
 function backup -d "backup"
     rsync -avL --delete ~/MyCode/ansible/ /mnt/DATA2/UbuntuBackup/ansible
@@ -59,6 +70,7 @@ function backup -d "backup"
     rsync -avL --delete ~/.goldendict/dict/ /mnt/DATA2/UbuntuBackup/Files/dict
     rsync -avL --delete /mnt/DATA2/UbuntuBackup/Files/deb/ ~/pCloudDrive/UbuntuBackup/deb
 end
+
 function data_backup -d "data backup"
     rsync -avL --delete /mnt/DATA1/itunes/ /mnt/EXHDD1/DataBackup/itunes
     rsync -avL --delete /mnt/DATA1/画像/ /mnt/EXHDD1/DataBackup/画像
@@ -71,16 +83,24 @@ function data_backup -d "data backup"
     rsync -avL --delete /mnt/DATA1/USB/ /mnt/EXHDD2/DataBackup/USB
     rsync -avL --delete /mnt/DATA1/VIDEO/ /mnt/EXHDD2/DataBackup/VIDEO
 end
+
+# fish
 function view_fish -d "view fish settings"
     cat ~/.config/fish/config.fish
 end
-function edit_fish -d "edit fish settings"
-    vim ~/.config/fish/config.fish
-end
+
 function update_fish -d "update fish settings"
     source ~/.config/fish/config.fish
 end
+
+function edit_fish -d "edit fish settings"
+    vim ~/.config/fish/config.fish
+    update_fish
+end
+
 function view_functions -d "list functions"
     grep "^function" ~/.config/fish/config.fish | awk -F'[ ]' -v 'ORS=\n' 'BEGIN {print "=======Funcsions======="} {print $2,$4,$5,$6} END {print "==========END=========="}'
 end
+
+# byobu settings
 status --is-login; and status --is-interactive; and exec byobu-launcher
