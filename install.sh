3848db65e70154f96c72fc97f84123aa64e8c302
fenrirpride <fenrirpride@gmail.com>
2020-01-17 21:19:13 +0900

    change install.sh

--- install.sh
+++ install.sh
@@ -3,34 +3,44 @@
 # エラーで停止
 set -e
 
-# dein
-ln -sf ~/MyCode/dotfiles/dein/dein.toml ~/.vim/dein
-ln -sf ~/MyCode/dotfiles/dein/dein-lazy.toml ~/.vim/dein
-
-# Code
-ln -sf ~/MyCode/dotfiles/Code/settings.json ~/.config/Code/User
-ln -sf ~/MyCode/dotfiles/Code/keybindings.json ~/.config/Code/User
-
-# install dotfiles
-sudo ln -sf ~/MyCode/dotfiles/xkeysnail /etc/opt/xkeysnail
-
 # set PATH
 DOTFILES=~/MyCode/dotfiles
+MYREPOSITORY=~/git/github.com/fenrirpride
+
+# dein
+ln -sf $DOTFILES/dein/dein.toml ~/.vim/dein
+ln -sf $DOTFILES/dein/dein-lazy.toml ~/.vim/dein
+
+# Code
+ln -sf $DOTFILES/Code/settings.json ~/.config/Code/User
+ln -sf $DOTFILES/Code/keybindings.json ~/.config/Code/User
+
+# install dotfiles
+sudo ln -sf $DOTFILES/xkeysnail /etc/opt/xkeysnail
 
 # make symbolic link at home
-cd $DOTFILES/.config
-for f in *
+cd $DOTFILES/home
+for f in .??*
 do
-    [ "$f" = ".git" ] && continue
-    ln -sfv "$DOTFILES"/.config/"$f" "$HOME"/.config/"$f"
+    ln -sfv "$DOTFILES/home/$f" "$HOME"
     # echo $f
 done
 
 # make symbolic link at .config
-cd $DOTFILES/home
-for f in .??*
+cd $DOTFILES/.config
+for f in *
 do
     [ "$f" = ".git" ] && continue
-    ln -sfv "$DOTFILES"/home/"$f" "$HOME"/"$f"
+    ln -sfv "$DOTFILES/.config/$f" "$HOME/.config"
     # echo $f
 done
+
+# make symbolic link at MyCode
+cd $MYREPOSITORY
+for f in *
+do
+    ln -sfv "$MYREPOSITORY/$f" "$HOME/MyCode"
+    # echo $f
+done
+
+ln -sfv "$HOME/pCloudDrive/wordpress" "$HOME/MyCode"
